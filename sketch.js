bf263f3933b3d9819339e8e3abd9f309379fe3ec
pietro <pietrof@live.com>
2020-01-13 00:45:53 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -164,10 +164,13 @@
           var amp = map(this.amplitude, 0, 255, 0, -340);
           var amp2 = map(this.amplitude, 0, 255, 0, -480);
 
+          (cos(angle) * radius + add) + centerX
+          (-sin(angle) * radius + add) + centerY
+
           strokeWeight(2);
-          point((cos(angle) * radius + amp2) + centerX, (-sin(angle) * radius + amp2) + centerY)
-          line(this.xVal, this.yVal, (cos(angle) * radius + amp) + centerX, (-sin(angle) * radius + amp) + centerY);
-          point((cos(angle) * radius + amp * 3) + centerX, (-sin(angle) * radius + amp*3) + centerY)
+          point((cos(angle) * (radius + amp2)) + centerX, (-sin(angle) * (radius + amp2)) + centerY)
+          line(this.xVal, this.yVal, (cos(angle) * (radius + amp)) + centerX, (-sin(angle) * (radius + amp)) + centerY);
+          point((cos(angle) * (radius + amp * 3)) + centerX, (-sin(angle) * (radius + amp * 3)) + centerY)
 
       }
   }
